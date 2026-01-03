6fb94ff80d817c8c37f606689b4fdcbb37fd7156
Boris Damevin <borisdamevin@gmail.com>
2026-01-03 23:59:43 +0100

    ci: add `release.config.js`

---
+++ release.config.js
@@ -0,0 +1,54 @@
+// ============================================================================================= //
+//                                            RELEASE                                            //
+// ============================================================================================= //
+
+export default {
+    branches: [
+        'main',
+        '+([0-9])?(.{+([0-9]),x}).x',
+        {
+            name: 'beta',
+            prerelease: true
+        }
+    ],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits'
+            }
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    types: [
+                        {
+                            type: 'feat',
+                            section: 'Features'
+                        },
+                        {
+                            type: 'fix',
+                            section: 'Bug Fixes'
+                        }
+                    ]
+                }
+            }
+        ],
+        [
+            '@semantic-release/changelog',
+            {
+                changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file. See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.'
+            }
+        ],
+        '@semantic-release/npm',
+        '@semantic-release/github',
+        [
+            '@semantic-release/git',
+            {
+                message: 'chore(release): publish version ${nextRelease.version}'
+            }
+        ]
+    ]
+};
